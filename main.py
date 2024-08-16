0ef1398ae1031ea8871ef4ef4e54729c1dfc80a8
basic-kali-box <basic.kali.box@gmail.com>
2024-08-16 14:18:00 -0400

    Intial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+from os.path import splitext
+import shutil
+import glob
+
+# Define the dictionary with extensions as keys and descriptive names as values
+extension_names = {
+    "txt": "Text Files",
+    "pdf": "PDF Documents",
+    "jpg": "JPG Pictures",
+    "jpeg": "JPEG Pictures",
+    "png": "PNG Pictures",
+    "docx": "Word Documents",
+    "xlsx": "Excel Spreadsheets",
+    "pptx": "PowerPoint Presentations",
+    "mp3": "MP3 Audio",
+    "mp4": "MP4 Video",
+    "html": "HTML Document",
+    "css": "CSS Stylesheet",
+    "js": "JavaScript File",
+    "rar": "Compressed Files",
+    "zip": "Compressed Files",
+    "py": "Python Files",
+    "sh": "Bash Scripts"
+}
+
+def add_non_repetetive(my_string, my_list):
+    if my_string and my_string[1:] not in my_list:
+        my_list.append(my_string[1:])
+
+def create_dir(my_list, paths):
+    base_paths = {}
+    for c in my_list:
+        if c not in extension_names:
+            new_dir = c.upper() + "_Files"
+        else:
+            new_dir = extension_names.get(c).replace(" ", "_")
+
+        # Ensure unique directory name
+        base_path = os.path.join(paths, new_dir)
+        counter = 1
+
+        while os.path.exists(base_path):
+            base_path = os.path.join(paths, f"{new_dir}_{counter}")
+            counter += 1
+        os.mkdir(base_path)
+        print(f"Created directory: {base_path}")
+        base_paths[c] = base_path
+
+    return base_paths
+
+def moving_files(dir, ext, base_paths):
+    for e in ext:
+        allfiles = glob.glob(os.path.join(dir, f'*.{e}*'))
+        print("Moving files: ", os.path.basename(allfiles))
+        for p in allfiles:
+            dst_path = os.path.join(base_paths[e], os.path.basename(p))
+            shutil.move(p, dst_path)
+
+while True:
+    directory = input("Enter the directory path: ").strip()
+    if os.path.isdir(directory):
+        if len(os.listdir(directory)) == 0:
+            print("The directory is empty, try again.")
+        else:
+            print("\n+ Starting the program...")
+            break  # Exit the loop once a valid directory is found
+    else:
+        print("Invalid path, try again.\n")
+
+# Collect unique file extensions
+extension = []
+for f in os.listdir(directory):
+    add_non_repetetive(splitext(f)[1], extension)
+
+# Create directories and get paths
+base_paths_returned = create_dir(extension, directory)
+
+# Move files to their respective directories
+moving_files(directory, extension, base_paths_returned)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+the only modules needed are "os" "shitil" "glob" , probably you already have them ,
+if you have them there's no need to **pip install -r requirements**
